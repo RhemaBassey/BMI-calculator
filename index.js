6ae4123b385e2e396b7aaee597c47f3c7e41dfe4
unknown <bassey.rhema@yahoo.com>
2024-09-12 11:55:37 +0100

    enableheight and weight unit functionality

--- index.js
+++ index.js
@@ -5,14 +5,24 @@ document.addEventListener("DOMContentLoaded", function () {
       event.preventDefault();
 
       var weight = parseFloat(document.getElementById('weightInput').value);
+      var weightUnit = document.getElementById('weightUnit').value;
+      console.log(weightUnit)
       var height = parseFloat(document.getElementById('heightInput').value);
-
+      var heightUnit = document.getElementById('heightUnit').value;
+      console.log(heightUnit)
+      
+      // convert to metric
+      if (weightUnit == "imperial"){
+        weight *= 0.45359237 
+      }
+      if (heightUnit == "imperial"){
+        height *= 0.3048
+      }
       var BMI = weight / height ** 2;
            var BMIrange = {"0.0":"Underweight", "18.5":"Normal weight", "25.0":"Overweight", "30.0":"Moderate Obesity", "35.0":"Severe Obesity", "40.0":"Morbid Obesity"}
 	
 	var category = ""
       for (keyWeight in BMIrange){
-	console.log(keyWeight)
             if (parseFloat(BMI.toFixed(2))< keyWeight){
 			break;
 	}
